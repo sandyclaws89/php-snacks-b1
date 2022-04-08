0e459d3cb2aec14794d2c1cc0835d517491e763e
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-04-08 15:56:19 +0200

    non capisco perché non mi stampa con echo

--- index.php
+++ index.php
@@ -7,6 +7,52 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+    /*SNACK1 */
+        /* Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+        Stampiamo a schermo tutte le partite con questo schema
+        OLIMPIA MILANO - CANTU' | 55-60
+            */
+        $array_partite = [
+            [
+                'casa' => [
+                    'nome' =>'Cantù',
+                    'punteggio' => 90,
+                ],
+                'ospite' => [
+                    'nome' =>'Olimpia Milano',
+                    'punteggio' => 80,
+                ],
+            ],
+            [
+                'casa' => [
+                    'nome' =>'Brindisi',
+                    'punteggio' => 100,
+                ],
+                'ospite' => [
+                    'nome' =>'Genova',
+                    'punteggio' => 69,
+                ],
+            ]
+        ];
+        ?>
+        <?php
+        $arr_match = [];
+        for ($_i=0; $_i < count($array_partite); $_i++) {
+           
+            $array_team = array_column($array_partite[$_i], 'nome');
+            // print_r($array_team); 
+            echo ($array_team);
+            // echo $match[$_i];
+            // $score = array_column($array_partite[$_i], 'punteggio');
+            // print_r($score);
+            
+           
+        //    $home_team
+        //    echo $home_team;
+            // echo join("-",$array_partite[$_i]);
+        }
+        // echo $match;
+   ?>
 </body>
 </html>
